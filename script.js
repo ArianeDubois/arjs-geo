07fa3c65fa3983ae76aec5651e9251a86f945783
ArianeDubois <36155355+ArianeDubois@users.noreply.github.com>
2022-06-17 01:10:03 +0200

    First display font

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ document.querySelector('a-entity').sceneEl.object3D;
 AFRAME.registerComponent('pointlight', {
     init: function () {
         this.el.setObject3D('light', new THREE.PointLight());
-        entityEl.getObject3D('light');
-        entityEl.getObject3D('light').el;  // entityEl
     }
 });
