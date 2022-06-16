98376fd70e8e94bc6d9c8d3e55882fc758326ee6
ArianeDubois <36155355+ArianeDubois@users.noreply.github.com>
2022-06-17 01:48:01 +0200

    Display 3d cypher font

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.querySelector('a-scene').object3D;
-document.querySelector('a-entity').sceneEl.object3D;
-AFRAME.registerComponent('pointlight', {
-    init: function () {
-        this.el.setObject3D('light', new THREE.PointLight());
-    }
-});
+// document.querySelector('a-scene').object3D;
+// document.querySelector('a-entity').sceneEl.object3D;
+// AFRAME.registerComponent('pointlight', {
+//     init: function () {
+//         this.el.setObject3D('light', new THREE.PointLight());
+//     }
+// });
+
+require('aframe');
+require('aframe-text-geometry-component');
+
+var fontJson = require('cypher.json');
+var el = document.createElement('a-entity');
+el.setAttribute('text', { font: fontJson });
